e85070767ac7b3f24d03caa8bcb0fe6dbd34da53
Jen Bird <jenniredfield@hotmail.com>
2020-11-07 21:09:25 +0000

    Finished moxios module

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -1,3 +1,7 @@
+import moxios from 'moxios';
+import { storeFactory } from '../../test/testUtils';
+import { getSecretWord } from './';
+
 import {actionTypes, correctGuess} from './';
 
 describe('correctGuess', () => {
@@ -6,4 +10,32 @@ describe('correctGuess', () => {
 
         expect(action).toEqual({type: actionTypes.CORRECT_GUESS})
     })
+});
+
+describe('getSecretWord action creator', () => {
+    beforeEach(() => {
+        moxios.install();
+    });
+    afterEach(() => {
+        moxios.uninstall();
+    });
+
+    test('adds response word to state', () => {
+        const secretWord = 'party';
+        const store = storeFactory();
+
+        moxios.wait(() => {
+            const request = moxios.requests.mostRecent();
+            request.respondWith({
+                status: 200,
+                response: secretWord
+            });
+        });
+
+        return store.dispatch(getSecretWord())
+            .then(() => {
+                const newState = store.getState();
+                expect(newState.secretWord).toBe(secretWord);
+            });
+    });
 })
